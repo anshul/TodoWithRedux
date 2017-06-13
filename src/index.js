3f49cc4c3a33ad351fd8f4f257bf524733ce2f5a
Anshul Khandelwal <anshul@anshul.io>
2017-06-13 10:59:28 +0530

    Adds default files and changes reducers

--- src/index.js
+++ src/index.js
@@ -1,22 +1,17 @@
-import React from 'react'
-import ReactDom from 'react-dom'
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
+import React from 'react';
+import ReactDom from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import RootReducer from './reducers/index';
+import reducers from './reducers';
 
-import App from './components/App'
+import App from './components/App';
 
-let store = createStore(RootReducer)
+let store = createStore(reducers);
 
 ReactDom.render(
   <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
-)
-
-// ReactDom.render(
-//     <App />,
-//   document.getElementById('root')
-// )
+);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,51 +1,41 @@
 import { combineReducers } from 'redux';
 import { CREATE_TODO, TOGGLE_TODO } from '../actions/index'
 
-const initialState = {todos : [
-  {task : 'task 1',isCompleted: true},
-  {task : 'task 2',isCompleted: false},
-  {task : 'task 3',isCompleted: false}
-]};
+const initialState = [
+  {id: 1, task : 'task 1',isCompleted: true},
+  {id: 2, task : 'task 2',isCompleted: false},
+  {id: 3, task : 'task 3',isCompleted: false}
+];
+
+const todoItem = (state = {}, action ) => {
+  switch(action.type){
+    case CREATE_TODO :
+      return { ...state, id: (new Date() / 1), task : action.payload , isCompleted : false };
+    case TOGGLE_TODO :
+     if (state.id !== action.payload) {
+          return state
+        };
+        return { ...state, isCompleted: !state.isCompleted };
+    default:
+      return state;
+  }
+}
 
 const TodoList = (state = initialState , action) => {
-	switch(action.type){
-		case CREATE_TODO :
-			return { ...state , todos :[...state.todos ,{task : action.payload , isCompleted : false }]}
-		case TOGGLE_TODO :
-			return { ...state , todos :[ ...state.todos, state.todos.map( todo => todoItem(todo,action) ) ]};
-		// case TOGGLE_TODO :
-		// 	return { ...state , 
-		// 			todos :[ ...state.todos , state.todos.find((todo) => {
-		// 				if(todo.task === action.payload){
-		// 					todo.isCompleted = !todo.isCompleted;
-		// 				}
-		// 			})
-		// 		]};
-		default:
-			return state;
-	}
+  switch(action.type){
+    case CREATE_TODO :
+      return [...state,  todoItem(undefined, action)];
+    case TOGGLE_TODO :
+      return state.map(todo => todoItem(todo, action));
+      // return { ...state , todos: [ ...state.todos, state.todos.map( todo => todoItem(todo,action) ) ]};
+    default:
+      return state;
+  }
 }
 
-const todoItem = (state = [] , action ) => {
-	switch(action.type){
-		case TOGGLE_TODO :
-		// console.log("STATE"+JSON.stringify(state));
-		// console.log("ACTION"+JSON.stringify(action));
-		 if (state.task !== action.payload) {
-	        return state
-	      }
-
-	      return Object.assign({}, state, {
-	        isCompleted: !state.isCompleted
-	      })
-		default:
-			return state;
-	}
-}
 
 const RootReducer = combineReducers({
   TodoList,
-  todoItem
 })
 
 export default RootReducer;
